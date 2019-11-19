731f12f8592e4532ed605966b87b3bb26f19c9c8
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-19 02:23:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,9 @@ monopoly = {
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => {}
+	      :reading_railroad => {},
+	      :pennsylvania_railroad => {},
+	      
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
